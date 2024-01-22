c86d8eccdba3febbd691031f2ac99f564ef715c4
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-22 12:20:40 -0500

    function added

--- capital_indexes.py
+++ capital_indexes.py
@@ -1,4 +1,10 @@
 # The function takes a single parameter, which is a string. 
-# Function returns a list of all indexes in the string, that have capital letters.
+# Function returns a list of all indices in the string that have capital letters.
 
 # E.g, paremeter "HeLlO", should return [0, 2, 4]
+
+def capital_indexes(string): 
+    indices =  [x for x, index in enumerate([ i.isupper() for i in list(string) ]) if index != False]
+    return indices
+ 
+print(capital_indexes("HeLlO"))
