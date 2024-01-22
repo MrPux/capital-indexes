b36ff8d89bb74c4aea773dd38b91408eac20efb3
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-21 21:49:51 -0500

    Master

---
+++ capital_indexes.py
@@ -0,0 +1,4 @@
+# The function takes a single parameter, which is a string. 
+# Function returns a list of all indexes in the string, that have capital letters.
+
+# E.g, paremeter "HeLlO", should return [0, 2, 4]
