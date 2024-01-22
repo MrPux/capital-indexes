0e6549bcddec0d638e4b8cebe8f0ce26fc4bdd45
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-22 12:53:37 -0500

    Added a more shjorter version of the function that accomplishes the same results, the value of this is to make the code more clean, it also gives a visual comparasion of the ways to code the function.

--- capital_indexes.py
+++ capital_indexes.py
@@ -7,4 +7,12 @@ def capital_indexes(string):
     indices =  [x for x, index in enumerate([ i.isupper() for i in list(string) ]) if index != False]
     return indices
  
-print(capital_indexes("HeLlO"))
+print(capital_indexes("HeLlO")) #It prints [0, 2, 4]
+
+#------------------------------------------------ 
+
+#The following function is a more shorter version that I coded that acccomplishes the same results 
+def capital_indices(string):
+    indices = [x for x, index in enumerate(list(string)) if index.isupper()]
+    return indices
+print(capital_indices("HeLlO")) #It prints [1, 2, 4]
